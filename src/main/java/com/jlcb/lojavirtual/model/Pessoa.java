c7da8c8624edf716afdaac7e895246cdd949549a
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2021-11-19 23:25:41 -0300

    Criando classe de modelo Endereco

--- src/main/java/com/jlcb/lojavirtual/model/Pessoa.java
+++ src/main/java/com/jlcb/lojavirtual/model/Pessoa.java
@@ -1,14 +1,19 @@
 package com.jlcb.lojavirtual.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 
 @Entity
@@ -29,6 +34,9 @@ public abstract class Pessoa implements Serializable {
 	private String email;
 
 	private String telefone;
+
+	@OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Endereco> enderecos = new ArrayList<>();
 
 	public Pessoa() {
 
@@ -64,6 +72,14 @@ public abstract class Pessoa implements Serializable {
 
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
+	}
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
 	}
 
 	@Override
