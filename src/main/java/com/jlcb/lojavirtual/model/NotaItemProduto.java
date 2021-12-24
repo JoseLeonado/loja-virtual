7f660e4071bb77b5f8e965a1c5c58017849f83a6
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2021-12-24 02:54:59 -0300

    Realizando alterações

--- src/main/java/com/jlcb/lojavirtual/model/NotaItemProduto.java
+++ src/main/java/com/jlcb/lojavirtual/model/NotaItemProduto.java
@@ -2,6 +2,7 @@ package com.jlcb.lojavirtual.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
@@ -24,6 +25,7 @@ public class NotaItemProduto implements Serializable {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_item_produto")
 	private Long id;
 
+	@Column(nullable = false)
 	private Double quantidade;
 
 	@ManyToOne
--- src/main/java/com/jlcb/lojavirtual/model/PessoaJuridica.java
+++ src/main/java/com/jlcb/lojavirtual/model/PessoaJuridica.java
@@ -15,10 +15,13 @@ public class PessoaJuridica extends Pessoa {
 	@Column(nullable = false)
 	private String cnpj;
 	
+	@Column(nullable = false)
 	private String razaoSocial;
 	
+	@Column(nullable = false)
 	private String nomeFantasia;
 	
+	@Column(nullable = false)
 	private String inscricaoEstadual;
 	
 	private String inscricaoMunicipal;
